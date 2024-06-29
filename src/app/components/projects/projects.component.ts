c53b87516217b93d30bd40b2b9079db964f57fc8
BorjaRBEST <borjarodriguez2003@gmail.com>
2024-06-29 12:36:45 +0200

    Casi terminada la parte de proyectos, falta implementar lógicva de contacto, y agregar linkedin y github

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -5,8 +5,31 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
 
+  toggleProject(event: Event) {
+    const projectElement = event.currentTarget as HTMLElement;
+    const allProjects = document.querySelectorAll('.project');
+
+    allProjects.forEach(project => {
+      if (project !== projectElement) {
+        project.classList.remove('expanded');
+        const content = project.querySelector('.content') as HTMLElement;
+        content.style.maxHeight = '0';
+        content.style.opacity = '0';
+      }
+    });
+
+    projectElement.classList.toggle('expanded');
+    const content = projectElement.querySelector('.content') as HTMLElement;
+    if (projectElement.classList.contains('expanded')) {
+      content.style.maxHeight = `${content.scrollHeight}px`;
+      content.style.opacity = '1';
+    } else {
+      content.style.maxHeight = '0';
+      content.style.opacity = '0';
+    }
+  }
 }
